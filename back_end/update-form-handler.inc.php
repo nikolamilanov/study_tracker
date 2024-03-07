43b7459829e8bb0208f176ff3c0b06d4b0d84e09
BgNikoto11 <nikolamilanov2005@gmail.com>
2024-03-07 12:31:10 +0200

    added update form

---
+++ back_end/update-form-handler.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $itemId = $_POST["item_id"];
+    $itemContent = $_POST["content"];
+    //$confidenceLevel = $_POST["confidence-level"];
+
+    try{
+        require_once "data-base-handler.inc.php";
+
+        $sql = " UPDATE items SET content = ? WHERE id = ?;";
+
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->execute([$itemContent,$itemId]);
+
+        $pdo = null;
+        $stmt = null;
+
+        header("Location: ../front_end/markup/edit_page.php");
+        
+        die();
+    }
+    catch (PDOException $e){
+        die("Query failed:" . $e->getMessage());
+    }
+
+    header("Location: ../front_end/markup/edit_page.php");   
+}
+else{
+header("Location: ../front_end/markup/edit_page.php");
+}
